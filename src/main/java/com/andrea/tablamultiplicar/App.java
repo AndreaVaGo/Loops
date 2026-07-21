151f264cb26729543eb63011c0e8a04840d403ea
AndreaVaGo <andrea.vallina.glez@gmail.com>
2026-07-21 09:47:05 +0200

    Set up Maven project with JUnit 5, Hamcrest and JaCoCo

---
+++ src/main/java/com/andrea/tablamultiplicar/App.java
@@ -0,0 +1,13 @@
+package com.andrea.tablamultiplicar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
