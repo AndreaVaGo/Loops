2ae9c02a545b7e0536708c73d607f75dae9d60b9
AndreaVaGo <andrea.vallina.glez@gmail.com>
2026-07-22 10:00:19 +0200

    Implement generateTable method with exception handling

--- src/main/java/com/andrea/tablamultiplicar/MultiplicationTable.java
+++ src/main/java/com/andrea/tablamultiplicar/MultiplicationTable.java
@@ -1,11 +1,25 @@
 
-
 package com.andrea.tablamultiplicar;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MultiplicationTable {
 
     public int multiply(int n, int i) {
         return n * i;
+    }
+
+    public List<String> generateTable(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("mensaje explicando el error");
+        }
+        List<String> listOfLines = new ArrayList<>();
+        for (int i = 1; i <= 10; i++) {
+            int result = multiply(n, i);
+            listOfLines.add(n + " x " + i + " = " + result);
+        }
+        return listOfLines;
     }
 
 }
--- src/test/java/com/andrea/tablamultiplicar/MultiplicationTableTest.java
+++ src/test/java/com/andrea/tablamultiplicar/MultiplicationTableTest.java
@@ -3,6 +3,11 @@ package com.andrea.tablamultiplicar;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
 
 public class MultiplicationTableTest {
 
@@ -13,4 +18,19 @@ public class MultiplicationTableTest {
         assertEquals(expected, table.multiply(n, i));
     }
 
+    @Test
+    public void shouldGenerateTable() {
+        MultiplicationTable table = new MultiplicationTable();
+        List<String> result = table.generateTable(5);
+        assertEquals(10, result.size());
+        assertEquals("5 x 3 = 15", result.get(2));
+
+    }
+
+    @ParameterizedTest
+    @CsvSource({ "-1", "-5", "-100" })
+    public void shouldThrowExceptionWhenNegative(int n) {
+        MultiplicationTable table = new MultiplicationTable();
+        assertThrows(IllegalArgumentException.class, () -> table.generateTable(n));
+    }
 }
